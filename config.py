6c3f00370ae57ebdef262ce7843a5c6e2e2e7708
bostonoly <bostonoly@gmail.com>
2026-03-19 18:37:08 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+BOT_TOKEN = "8717799900:AAGzQF543tZghfsdI5VBIE88zxyaxl3-wmI"
+CHANNEL_ID = -1003718460448
+MANAGER_ID = 8696211185
+```
+
+---
+
+**requirements.txt**
+```
+aiogram==3.7.0
